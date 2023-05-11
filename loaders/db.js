fb379a02f131f31d4cc5cf0593573ce37fe5bde4
Negasi <negasi@Negasis-MacBook-Air.local>
2023-05-12 02:05:57 +0300

    Minor changes

--- loaders/db.js
+++ loaders/db.js
@@ -8,7 +8,7 @@ const config = require("../config");
 const options = {
   autoIndex: false, // Don't build indexes
   maxPoolSize: 10, // Maintain up to 10 socket connections
-  serverSelectionTimeoutMS: 50000, // Keep trying to send operations for 5 seconds
+  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
   socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
   // family: 4, // Use IPv4, skip trying IPv6
 };
